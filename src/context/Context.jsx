f291edabd8691ec76044580318e4eb3a6433566a
techfreakSahil <gm6504@myamu.ac.in>
2024-04-18 17:06:43 +0530

    🚀 Created ContextAPI

---
+++ src/context/Context.jsx
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+import runChat from "../generative-ai.config";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPropmt, setPrevPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    const response = await runChat(input);
+    setResultData(response);
+    setLoading(false);
+    setInput("");
+  };
+  const contextValue = {
+    prevPropmt,
+    setPrevPrompt,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    input,
+    setInput,
+    resultData,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
