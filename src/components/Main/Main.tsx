498f532433d3a2b9d5abc2ae8aadd9c87c1c4026
techfreakSahil <gm6504@myamu.ac.in>
2024-04-18 17:08:04 +0530

    🚀 Updated UI for showing prompt result

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,17 @@
 import "./Main.css";
 import { assets } from "../../assets/assets";
+import { useContext } from "react";
+import { Context } from "../../context/Context.jsx";
 const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    input,
+    setInput,
+    resultData,
+  }: any = useContext(Context);
   return (
     <div className="main">
       <div className="nav">
@@ -8,44 +19,74 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello, Sahil</span>
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Sahil</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest best company to work with after graduation</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Suggest best place to visit with after graduation</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Suggest best company to work with after graduation</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Suggest best company to work with after graduation</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result-container">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom">
+          <div className="searchbox">
+            <input
+              type="text"
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              placeholder="Enter a prompt here"
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img onClick={() => onSent()} src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info, including about people, so
+            double-check it before forwarding it.
           </p>
-          <p>How can I help you today?</p>
         </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest best company to work with after graduation</p>
-            <img src={assets.compass_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Suggest best place to visit with after graduation</p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Suggest best company to work with after graduation</p>
-            <img src={assets.message_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Suggest best company to work with after graduation</p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-        </div>
-      </div>
-      <div className="main-bottom">
-        <div className="searchbox">
-          <input type="text" placeholder="Enter a prompt here" />
-          <div>
-            <img src={assets.gallery_icon} alt="" />
-            <img src={assets.mic_icon} alt="" />
-            <img src={assets.send_icon} alt="" />
-          </div>
-        </div>
-        <p className="bottom-info">
-          Gemini may display inaccurate info, including about people, so
-          double-check it before forwarding it.
-        </p>
       </div>
     </div>
   );
